e9b048efdf04868f2ea31cedee3d82535cc04a86
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2024-02-13 16:43:19 +0100

    Complete

--- src/main/java/com/example/ApplicazioneBlogging/controller/BlogController.java
+++ src/main/java/com/example/ApplicazioneBlogging/controller/BlogController.java
@@ -3,12 +3,12 @@ package com.example.ApplicazioneBlogging.controller;
 import com.example.ApplicazioneBlogging.model.Blog;
 import com.example.ApplicazioneBlogging.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 //@RequestMapping("/api")
@@ -24,4 +24,28 @@ public class BlogController {
     public Blog getBlogByIdQueryParams(@RequestParam int id){
         return blogService.getBlogById(id);
     }
+    @GetMapping("/blog/{id}")
+    public ResponseEntity<Blog> getBlogByIdPathVariable(@PathVariable int id){
+        try {
+            Blog b = blogService.getBlogById(id);
+            return new ResponseEntity<Blog>(b, HttpStatus.OK);
+        }
+        catch (NoSuchElementException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+    @PostMapping("/blog")
+    public void saveBlog(@RequestBody Blog blog){
+        blogService.saveBlog(blog);
+    }
+    @PutMapping("/blog/{id}")
+    public Blog updateBlog(@PathVariable int id, @RequestBody Blog blog){
+        return blogService.aggiornaBlog(id, blog);
+    }
+    @DeleteMapping("/blog/{id}")
+    public void deleteBlog(@PathVariable int id){
+        blogService.deleteBlog(id);
+    }
+
 }
